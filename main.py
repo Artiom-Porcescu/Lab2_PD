3b319f1df9f2464f977781165064b03d93abead5
Artiom Porcescu <43860271+Artiom-Porcescu@users.noreply.github.com>
2023-10-16 09:32:11 +0300

    Refactored code into a function which has 2 parameters, start_date and end_date in order to make code more readable and customizable

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import requests
 import pandas as pd
 
+def get_usd_exchange_rates(start_date, end_date):
+    exchange_rates = []
+
+    base_url = "https://www.cbr-xml-daily.ru/archive/"
+
+    current_date = start_date
+    while current_date <= end_date:
+        date_str = current_date.strftime("%Y/%m/%d")
+        url = f"{base_url}{date_str}/daily_json.js"
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            usd_rate = data['Valute']['USD']['Value']
+            exchange_rates.append((date_str, usd_rate))
+
+        current_date += pd.Timedelta(days=1)
+
+    return exchange_rates
+
 start_date = pd.to_datetime("2023-09-01")
 end_date = pd.to_datetime("2023-09-30")
 
-exchange_rates = []
+usd_rates = get_usd_exchange_rates(start_date, end_date)
 
-base_url = "https://www.cbr-xml-daily.ru/archive/"
-
-while start_date <= end_date:
-    date_str = start_date.strftime("%Y/%m/%d")
-    url = f"{base_url}{date_str}/daily_json.js"
-
-    response = requests.get(url)
-    if requests.get(url).status_code == 200:
-        data = requests.get(url).json()
-        usd_rate = data['Valute']['USD']['Value']
-        exchange_rates.append((date_str, usd_rate))
-
-    start_date += pd.Timedelta(days=1)
-
-df = pd.DataFrame(exchange_rates, columns=["Date", "USD_Rate"])
+df = pd.DataFrame(usd_rates, columns=["Date", "USD_Rate"])
 
 df.to_csv("dataset.csv", index=False)
+
+print("Данные сохранены в dataset.csv”)
