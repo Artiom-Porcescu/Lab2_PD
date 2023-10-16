6ea325629ca19fa51f630d43ca1cc636af6f6265
Artiom Porcescu <43860271+Artiom-Porcescu@users.noreply.github.com>
2023-10-16 09:18:09 +0300

    first iteration of main code implemented

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+
+start_date = pd.to_datetime("2023-09-01")
+end_date = pd.to_datetime("2023-09-30")
+
+exchange_rates = []
+
+base_url = "https://www.cbr-xml-daily.ru/archive/"
+
+while start_date <= end_date:
+    date_str = start_date.strftime("%Y/%m/%d")
+    url = f"{base_url}{date_str}/daily_json.js"
+
+    data = requests.get(url).json()
+    usd_rate = data['Valute']['USD']['Value']
+    exchange_rates.append((date_str, usd_rate))
+
+    start_date += pd.Timedelta(days=1)
+
+df = pd.DataFrame(exchange_rates, columns=["Date", "USD_Rate"])
+
+df.to_csv("dataset.csv", index=False)
